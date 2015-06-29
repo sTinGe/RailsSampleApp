aedbd5575aac57982e1834735da3d7244681d84d
sTinGe <stinge62580@gmail.com>
2015-06-29 22:31:28 +0800

    add contact page & modified using named route

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,38 +4,47 @@ describe "StaticPages" do
   describe "Home Page" do
     it "should have the content 'Sample App'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/home'
+      visit 'static_pages/home'
       expect(page).to have_content('Sample App')
     end
-    it "should have the title 'Home'" do
-      visit '/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
-    end
     it "Should not have a custom title" do
-      visit '/home'
+      visit 'static_pages/home'
       expect(page).not_to have title('| home')
     end
   end
 
   describe "Help page" do
     it "should have the content 'Help'" do
-      visit '/help'
+      visit 'static_pages/help'
       expect(page).to have_content('Help')
     end
-    it "should have the content 'Help'" do
-      visit '/help'
+    it "should have the title 'Help'" do
+      visit 'static_pages/help'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
     end
   end
 
   describe "About page" do
     it "should have the content 'About Us'" do
-      visit '/about'
+      visit 'static_pages/about'
       expect(page).to have_content('About')
     end
-    it "should have the content 'About Us'" do
-      visit '/about'
+    it "should have the title 'About Us'" do
+      visit 'static_pages/about'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
+    end
+  end
+
+  describe "Contact page" do
+    
+    it "should have the content 'Contact'" do
+      visit 'static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have title 'Contact'" do
+      visit 'static_pages/contact'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
     end
   end
 end
