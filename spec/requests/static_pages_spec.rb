cb7feb0c621f60032569fc6c1b0889e61abf0f84
sTinGe <stinge62580@gmail.com>
2015-06-30 23:02:46 +0800

    refactor static_pages_spec.rb

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,50 +1,34 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  subject { page }
+
   describe "Home Page" do
-    it "should have the content 'Sample App'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit root_path
-      expect(page).to have_content('Sample App')
-    end
-    it "Should not have a custom title" do
-      visit root_path
-      expect(page).not_to have title('| home')
-    end
+    before { visit root_path }
+    
+    it { should have_content('Sample App') }
+    it { should have_title("Ruby on Rails Tutorial Sample App") }
+    it { should_not have_title('| Home') }
   end
 
   describe "Help page" do
-    it "should have the content 'Help'" do
-      visit help_path
-      expect(page).to have_content('Help')
-    end
-    it "should have the title 'Help'" do
-      visit help_path
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
-    end
+    before { visit help_path }
+
+    it { should have_content('Help') }
+    it { should have_title('Help') }
   end
 
   describe "About page" do
-    it "should have the content 'About Us'" do
-      visit about_path
-      expect(page).to have_content('About')
-    end
-    it "should have the title 'About Us'" do
-      visit about_path
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
-    end
+    before { visit about_path }
+
+    it { should have_content('About Us') }
+    it { should have_title('About Us') }
   end
 
   describe "Contact page" do
-    
-    it "should have the content 'Contact'" do
-      visit contact_path
-      expect(page).to have_content('Contact')
-    end
+    before { visit contact_path }
 
-    it "should have title 'Contact'" do
-      visit contact_path
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
-    end
+    it { should have_content('Contact') }
+    it { should have_title('Contact') }
   end
 end
