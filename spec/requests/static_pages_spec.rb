c05b83c4f9ae666c59f75648ea0cc29c8a3d22e2
sTinGe <stinge62580@gmail.com>
2015-07-01 17:50:41 +0800

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,7 +7,7 @@ describe "StaticPages" do
     before { visit root_path }
     
     it { should have_content('Sample App') }
-    it { should have_title("Ruby on Rails Tutorial Sample App") }
+    it { should have_title(full_title('')) }
     it { should_not have_title('| Home') }
   end
 
@@ -15,20 +15,20 @@ describe "StaticPages" do
     before { visit help_path }
 
     it { should have_content('Help') }
-    it { should have_title('Help') }
+    it { should have_title(full_title('Help')) }
   end
 
   describe "About page" do
     before { visit about_path }
 
     it { should have_content('About Us') }
-    it { should have_title('About Us') }
+    it { should have_title(full_title('About Us')) }
   end
 
   describe "Contact page" do
     before { visit contact_path }
 
     it { should have_content('Contact') }
-    it { should have_title('Contact') }
+    it { should have_title(full_title('Contact')) }
   end
 end
