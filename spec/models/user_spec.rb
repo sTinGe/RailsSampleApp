2155fd752578a941b31601e3bd2efd116b23390f
sTinGe <stinge62580@gmail.com>
2015-07-04 14:58:55 +0800

    updating password validation & README.md

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,8 +14,9 @@ describe User do
   it { should respond_to(:name)  }
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
-  it { should respond_to(:password)}
-  it { should respond_to(:password_confirmation)}
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:authenticate) }
 
   it { should be_valid }
 
@@ -32,6 +33,7 @@ describe User do
   describe "when email address is already taken" do
     before do
       user_with_same_email = @user.dup
+      user_with_same_email.email = @user.email.upcase
       user_with_same_email.save
     end
 
@@ -49,7 +51,29 @@ describe User do
   describe "when password doesn't match confirmation" do
     before { @user.password_confirmation = "mismatch" }
 
-    it { should_not be_valid }
+    it { should be_invalid }
+  end
+
+  describe "with a password that's too short" do
+    before { @user.password = @user.password_confirmation = "a" * 5 }
+    
+    it { should be_invalid }
+  end
+
+  describe "return value of authenticate method" do
+    before { @user.save }
+    let(:found_user) { User.find_by(email: @user.email) }
+
+    describe "with valid password" do
+      it { should eq found_user.authenticate(@user.password) }
+    end
+
+    describe "with invalid password" do
+      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+      it { should_not eq user_for_invalid_password }
+      specify { expect(user_for_invalid_password).to be_falsey }
+    end
   end
 end
 
