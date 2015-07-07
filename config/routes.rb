4937a6e811e801eae35ad1175e8961e8b3845d0e
sTinGe <stinge62580@gmail.com>
2015-07-07 21:54:34 +0800

    add user resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  
-  get "users/new"
-
+  resources :users
   root to: 'static_pages#home'
+
   match '/signup',  to: 'users#new',            via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
