9fad3ae4026d7db5a3db479ccccbd08de06b6ebe
sTinGe <stinge62580@gmail.com>
2015-07-13 23:24:12 +0800

    add create method & user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,25 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
   def new
   	@user = User.new
   end
+
+  def create
+  	@user = User.new(user_params) # Not the final inplementation!
+  																	# it's a very dangerous method
+  	if @user.save
+  		# Handle a successful save.
+		else
+			render 'new'
+		end
+  end
+
+
+  private 
+  	def user_params
+  		params.require(:user).permit(:name, :email, :password, 
+  																 :password_confirmation)
+  	end
 end
