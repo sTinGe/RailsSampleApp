4d9493f3feb797969565ed5ac55d2929b2e53a4e
sTinGe <stinge62580@gmail.com>
2015-07-14 19:28:16 +0800

    fix Rspec signin integrated test

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destory]
   root to: 'static_pages#home'
 
   match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destory',     via: 'delete'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
